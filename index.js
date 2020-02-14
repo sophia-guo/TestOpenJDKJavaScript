1905c1bb1b6914676c9a97df4c5dc173d02f9f11
Sophia Guo <sophiag@ca.ibm.com>
2020-02-14 11:35:42 -0500

    Support github hosted machine default JDK
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
 
 async function runaqaTest(version, buildList, target) {
   process.env.BUILD_LIST = buildList
-  process.env.TEST_JDK_HOME = process.env.JAVA_HOME
+  process.env.TEST_JDK_HOME = defaultJAVAHome(version);
   await exec.exec('ls')
   await exec.exec(
     'git clone --depth 1 https://github.com/AdoptOpenJDK/openjdk-tests.git'
@@ -17,6 +18,17 @@ async function runaqaTest(version, buildList, target) {
   await exec.exec('make', [`${target}`])
 }
 
+function defaultJAVAHome(version) {
+  let defaultJavahome;
+  // On linux for now
+  if (process.platform === "linux") {
+    defaultJavahome = process.env[`JAVA_HOME_${version}_X64`];
+    core.info(` Javahome is ${defaultJavahome}`);
+  } else {
+    core.error(" TODO for other platform");
+  }
+  return defaultJavahome;
+}
 
 try {
   let version = core.getInput('version', {required: true})
